7df1206569cc38bf4c0b0f0ad90dfe6f65512545
TrajanoXT <viniciustrajano46@gmail.com>
2026-07-13 00:41:21 -0300

    chore: Creating the folder structure.

---
+++ src/test/java/spring/doc_flow/DocFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.doc_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
